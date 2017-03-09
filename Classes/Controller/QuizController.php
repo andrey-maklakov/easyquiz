1eb4c445a95b617726c43388fd79319ad2548378
Christopher Zechendorf <christopher@zechendorf.com>
2017-03-09 13:22:19 +0100

    0.8.0 basic functionality

--- Classes/Controller/QuizController.php
+++ Classes/Controller/QuizController.php
@@ -31,23 +31,139 @@ namespace ZECHENDORF\Easyquiz\Controller;
  */
 class QuizController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 {
+  
+  /**
+  * quizRepository
+  *
+  * @var \ZECHENDORF\Easyquiz\Domain\Repository\QuizRepository
+  * @inject
+  */
+  protected $quizRepository = NULL;
+  
+  /**
+  * quizParticipationRepository
+  *
+  * @var \ZECHENDORF\Easyquiz\Domain\Repository\QuizParticipationRepository
+  * @inject
+  */
+  protected $quizParticipationRepository = NULL;
+  
+  /**
+  * questionsRepository
+  *
+  * @var \ZECHENDORF\Easyquiz\Domain\Repository\QuestionsRepository
+  * @inject
+  */
+  protected $questionsRepository = NULL;
+  
+  /**
+  * answersRepository
+  *
+  * @var \ZECHENDORF\Easyquiz\Domain\Repository\AnswersRepository
+  * @inject
+  */
+  protected $answersRepository = NULL;
 
-    /**
-     * quizRepository
-     *
-     * @var \ZECHENDORF\Easyquiz\Domain\Repository\QuizRepository
-     * @inject
-     */
-    protected $quizRepository = NULL;
+  /**
+  * action display
+  *
+  * @return void
+  */
+  public function displayAction()
+  {
+    // configuration
+    $config = array('showExplanations'=>true);
     
-    /**
-     * action display
-     *
-     * @return void
-     */
-    public function displayAction()
-    {
+    // if there is a quiz defined in the plugin
+    if($this->settings['entries']){
+      // get the quiz
+      $uid = $this->settings['entries'];
+      $quiz = $this->quizRepository->findByUid($this->settings['entries']);
+      
+      
+      // default values for display and nextDisplay
+      $display = array('question'=>0,'showExplanation'=>0);
+      
+      // determine which question to show
+      if($this->request->hasArgument('display')){
+        $argumentDisplay = $this->request->getArgument('display');
+        $display['question'] = $argumentDisplay['question'];
+        $display['showExplanation'] =  $argumentDisplay['showExplanation'];
+      }
+      
+      // determin what to show next
+      if($config['showExplanations']&&!$display['showExplanation']){
+        $nextDisplay = array('question'=>$display['question'],'showExplanation'=>true);
+      } else {
+        $nextDisplay = array('question'=>$display['question']+1,'showExplanation'=>false);
+      }
+      
+      // get the quizParticipation
+      if($this->request->hasArgument('quizParticipation')){
+        $quizParticipation = $this->quizParticipationRepository->findByUid($this->request->getArgument('quizParticipation'));
+        $this->quizParticipationRepository->update($quizParticipation);
+      } else {
+        $quizParticipation = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('ZECHENDORF\\Easyquiz\\Domain\\Model\\QuizParticipation');
+        $this->quizParticipationRepository->add($quizParticipation);
+      }
+      
+      // get the answers
+      if($this->request->hasArgument('question') && is_array($this->request->getArgument('question'))){
+        foreach($this->request->getArgument('question') as $questionUid=>$questionResult){
+          // get the question
+          $question = $this->questionsRepository->findByUid($questionUid);
+          
+          // remove all answers to that question from quizParticipation
+          foreach($quizParticipation->getAnswers() as $answer){
+            foreach($question->getAnswers() as $questionAnswer){
+              if($questionAnswer->getUid()==$answer->getUid()){
+                $quizParticipation->removeAnswer($answer);
+              }
+            }
+          }
+          
+          // add new answers to quizParticipation
+          if(is_array($questionResult['answer'])){
+            // multiple answers
+            foreach($questionResult['answer'] as $answerUid){
+              $answer = $this->answersRepository->findByUid($answerUid);
+              $quizParticipation->addAnswer($answer);
+            }
+          } else{
+            $answer = $this->answersRepository->findByUid($questionResult['answer']);
+            $quizParticipation->addAnswer($answer);
+          }
+        }
+      }
+      
+      if($display['question']>=count($quiz->getQuestions())){
+        // get the participants points
+        $points = 0;
+        foreach($quizParticipation->getAnswers() as $answer){
+          $points += $answer->getPoints();
+        }
         
+        // check which result the participant matches 
+        foreach($quiz->getResults() as $result){
+          if($result->getMinPoints()<=$points && $result->getMaxPoints()>=$points){
+            // the point range matches - we redirect
+            $this->redirectToURI(
+              $this->uriBuilder->reset()
+              ->setTargetPageUid($result->getPage())
+              ->build()
+            );
+          }
+        }
+      }
+      
     }
+    $persistenceManager = $this->objectManager->get("TYPO3\\CMS\\Extbase\\Persistence\\Generic\\PersistenceManager");
+    $persistenceManager->persistAll();
+    $this->view->assign('quizParticipation',$quizParticipation);
+    $this->view->assign('config',$config);
+    $this->view->assign('display',$display);
+    $this->view->assign('nextDisplay',$nextDisplay);
+    $this->view->assign('quiz',$quiz);
+  }
 
 }
--- ext_tables.php
+++ ext_tables.php
@@ -11,6 +11,18 @@ if (!defined('TYPO3_MODE')) {
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Easy Quiz');
 
+
+/***************
+ * Reset extConf array to avoid errors
+ */
+if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY])) {
+	$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY] = serialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+}
+$pluginSignature = 'easyquiz_display';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue( $pluginSignature, 'FILE:EXT:easyquiz/Configuration/FlexForms/Display.xml');
+
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_easyquiz_domain_model_quiz', 'EXT:easyquiz/Resources/Private/Language/locallang_csh_tx_easyquiz_domain_model_quiz.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyquiz_domain_model_quiz');
 
